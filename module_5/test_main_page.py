b6131e0c1c4b666c752ea58c6fc9432d10ef3640
maddluxx <maddluxx@gmail.com>
2020-12-19 16:26:08 -0500

    Module 5 init.

---
+++ module_5/test_main_page.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium import webdriver
+
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+class TestMainPage:
+    def test_guest_can_go_to_login_page(self, browser):
+        browser.get(link)
+        login_link = browser.find_element_by_css_selector("#login_link")
+        login_link.click()
