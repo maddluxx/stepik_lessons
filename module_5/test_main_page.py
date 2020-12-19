2ff5672edcd83cbb59358e998c79634be9760625
maddluxx <maddluxx@gmail.com>
2020-12-19 17:58:23 -0500

    Module 5 tes_main_page POM update 1.

--- module_5/test_main_page.py
+++ module_5/test_main_page.py
@@ -1,5 +1,4 @@
-import pytest
-from selenium import webdriver
+from .pages.main_page import MainPage
 
 
 link = "http://selenium1py.pythonanywhere.com/"
@@ -7,6 +6,7 @@ link = "http://selenium1py.pythonanywhere.com/"
 
 class TestMainPage:
     def test_guest_can_go_to_login_page(self, browser):
-        browser.get(link)
-        login_link = browser.find_element_by_css_selector("#login_link")
-        login_link.click()
+        page = MainPage(browser,
+                        link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()  # открываем страницу
+        page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
